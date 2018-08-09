4f8a86d84f1539c7845b8b6e20792d01b527ab6b
Elaine Anderson <eparie@bu.edu>
2018-08-09 02:08:37 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,5 @@
 class ArtistsController < ApplicationController
   def index
-    binding.pry
     @artists = Artist.all
     if Preference.all.first.artist_sort_order != nil && Preference.all.first.artist_sort_order == "ASC"
       @artists = @artists.order('name ASC')
@@ -12,6 +11,7 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    binding.pry
     if Preference.all.first.allow_create_artists
     @artist = Artist.new
   else
