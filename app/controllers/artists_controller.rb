2034e89b9917d28e01438a9c2469421d61742d5d
Elaine Anderson <eparie@bu.edu>
2018-08-09 02:11:38 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
-    if !Preference.all.first.empty? && Preference.all.first.artist_sort_order == "ASC"
+    if !Preference.all.first.empty?
+      if Preference.all.first.artist_sort_order == "ASC"
       @artists = @artists.order('name ASC')
+    end
     end
   end
 
